64738912fd6c516b9f0e46f9ce982ea70144fd5f
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-04-19 23:11:09 +0530

    Config file change

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,10 @@ require('dotenv').config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_RPC_URL,
+      accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+  },
 };
